81e852c19f4f69eb74c06dfc0b4246c1b1c1e192
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 05:05:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
@@ -26,9 +26,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.float "price"
     t.string "image_url"
-    t.string "size"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
