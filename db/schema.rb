d5bf34a21ad0d9cf53d07f226f897cd5d74b3945
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 05:11:25 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.boolean "family_friendly"
     t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
   end
 
 end
