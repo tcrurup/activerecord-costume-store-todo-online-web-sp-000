de25dce37bb6af1284b2c2b3623e9ec4b3278088
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 04:46:44 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.string :img_url
+      t.string :image_url
     end
   end
 end
