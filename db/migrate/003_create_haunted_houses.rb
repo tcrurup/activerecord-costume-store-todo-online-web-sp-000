071fee08e6ce7bf491c070122b32319eef380a02
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 05:08:39 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
+      t.string :theme
     end
   end
 end
