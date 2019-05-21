34541cf327e8b74b7c0a22c3014a92d5c82e21f3
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 04:59:52 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
+      t.integer :num_of_employees
     end
   end
 end
