b7d4e07110dfa048c8147ae239cd15ff6035e422
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 05:08:57 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -26,10 +26,15 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "name"
     t.float "price"
     t.string "image_url"
+    t.string "size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
+    t.string "location"
+    t.string "theme"
   end
 
 end
