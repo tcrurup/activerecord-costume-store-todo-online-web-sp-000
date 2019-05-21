51be9b4479e0d2f0e8bed496f3c0865c5a0da61b
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 05:11:14 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,6 +7,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
     end
   end
 end
