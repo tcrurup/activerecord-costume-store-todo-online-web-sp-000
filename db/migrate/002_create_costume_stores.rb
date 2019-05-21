e41d525b28890f1c8912d7840fe18eb1ef9591e4
Anthony Rurup <tcrurup@gmail.com>
2019-05-21 05:03:20 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,6 +7,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
